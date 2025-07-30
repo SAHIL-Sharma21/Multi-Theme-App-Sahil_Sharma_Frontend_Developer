1ca26970e36a22a79d16194cec0340c161333c6d
Sahil <sahilsharma2000dtc@gmail.com>
2025-07-30 22:04:38 +0530

    Porject Init and tailwind setup

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+function App() {
+
+  return (
+    <>
+      <div>
+        <h2 className='text-4xl text-center text-blue-300'>Multi Theme switcher application</h2>
+      </div>
+    </>
+  )
+}
+
+export default App
